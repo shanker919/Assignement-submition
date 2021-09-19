b2e99f147c5a07cc3c17a226ba0e5edbd2648995
shanker919 <91017250+shanker919@users.noreply.github.com>
2021-09-19 19:42:40 +0530

    day 1 assignment pattens

---
+++ Pattern.java
@@ -0,0 +1,11 @@
+package _Learn;
+import java.util.Scanner;
+
+public class Pattern {
+	public static void Main(String[]arg) {
+		Scanner scan =new Scanner(System.in);
+		int n = scan.nextInt();
+		System.out.println(n);
+	}
+
+}
